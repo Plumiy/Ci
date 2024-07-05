3f687ec716a6e0c23d6c81d9d82a69e47c34c5c5
plumiy <slivinskysv@mail.ru>
2024-07-05 15:16:50 +0300

    05.07.2024

--- calculator.c
+++ calculator.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-// int calculator(int a, int b, char operator)
-// {
-//     if (operator=='+')
-//         {return a + b;}
-//     else if (operator=='-')
-//         {return a - b;}
-//     else if (operator=='*')
-//         {return a * b;}
-// }
+int calculator(int a, int b, char oper[10])
+{
+    if (oper=="+")
+        {return a + b;}
+    else if (oper=="-")
+        {return a - b;}
+    else if (oper=="*")
+        {return a * b;}
+}
 
 int main(void)
 {
 int chislo1; 
 int chislo2;
-char operator[1];
+char operator[10];
   
-    printf("enter x\t\n");    
+    printf("enter chislo1\t\nak\n");    
     scanf("%d %d", &chislo1, &chislo2);
-    scanf("%1s", operator);
-    printf("argument = %d%d\n", chislo1, chislo2);
-     printf("argument = %s\n", operator);
-    // int res = calculator(x, y, '+');
-    // printf("resultat = %d\n", res);
+    scanf("%10s", operator);
+    printf("argument = %d%s%d\n", chislo1, operator, chislo2);
+    //printf("argument = %s\n", operator);
+    //calculator(chislo1, chislo2, operator);
+    printf("resultat = %d\n", calculator(chislo1, chislo2, operator));
 return 0;
 }
